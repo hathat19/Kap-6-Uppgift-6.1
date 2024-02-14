c56a5943b59bc937901b1edec5c41a88f65164f3
Hugo M <humy06001@utb.vaxjo.se>
2024-02-14 12:24:18 +0100

    Add project files.

---
+++ Kap 6 Uppgift 6.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Addera(4, 5, 6));
+            Console.WriteLine(Addera(4,123));
+        }
+
+        static int Addera(int a = 0, int b = 0, int c = 0)
+        {
+            return a + b + c;
+        }
+    }
+}
+/*Skapa ett program som innehåller metoden
+static int Addera(int tal1, int tal2, int tal3)
+Metoden ska addera och returnera summan av de tre talen.*/
